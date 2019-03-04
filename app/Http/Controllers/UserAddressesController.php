c7a540aabcdf0151cf27772db259f8012957c06e
vagrant <vagrant@homestead>
2019-03-04 11:39:55 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAddress;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
@@ -13,4 +15,29 @@ class UserAddressesController extends Controller
             'addresses' => $request->user()->addresses,
         ]);
     }
+
+    // 新增收获地址
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', [
+            'address' => new UserAddress()
+        ]);
+    }
+
+    // 表单验证
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ Route::get('/', 'PagesController@root')->name('root');
 // 添加邮箱验证中间件：->middleware('verified')
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
+    // 收货地址页面
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    // 新增收货地址
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    // 表单验证
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
 
 
